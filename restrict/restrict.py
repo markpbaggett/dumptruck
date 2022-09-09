9150385481dc32bd563170c82a5e57c817c5fead
Mark Baggett <mbagget1@utk.edu>
2022-09-09 17:09:27 -0400

    Init restrict.

---
+++ restrict/restrict.py
@@ -0,0 +1,84 @@
+import magic
+import requests
+
+
+class FedoraObject:
+    def __init__(
+        self, fedora_url="http://localhost:8080", auth=("fedoraAdmin", "fedoraAdmin")
+    ):
+        self.fedora_url = fedora_url
+        self.auth = auth
+
+    def add_managed_datastream(
+        self,
+        pid,
+        dsid,
+        file,
+        versionable="true",
+        datastream_state="A",
+        checksum_type="DEFAULT",
+        alt_label=""
+    ):
+        """Adds an internally managed datastream.
+        This is not a one to one vesion of addDatastream.  It has been stripped down to fit one use case: internally
+        managed content.  I'll add other methods if other use cases arise.
+        Args:
+            pid (str): The persistent identifier to the object when you want to add a file.
+            dsid (str): The datastream id to assign your new file.
+            file (str): The path to your file.
+            versionable (str): Defaults to "true".  Specifies whether the datastream should have versioning ("true" or "false").
+            datastream_state (str): Specify whether the datastream is active, inactive, or deleted.
+            checksum_type (str): The checksum type to use.  Defaults to "DEFAULT". See API docs for options.
+        Returns:
+            int: The http status code of the request.
+        Examples:
+            >>> FedoraObject().add_managed_datastream("test:10", "AIP", "my_aip.7z")
+            201
+        """
+        if checksum_type not in (
+            "DEFAULT",
+            "DISABLED",
+            "MD5",
+            "SHA-1",
+            "SHA-256",
+            "SHA-385",
+            "SHA-512",
+        ):
+            raise Exception(
+                f"\nInvalid checksum type specified for {pid} when adding the {dsid} datastream with {file}"
+                f"content.\nMust be one of: DEFAULT, DISABLED, MD5, SHA-1, SHA-256, SHA-385, SHA-512."
+            )
+        if alt_label == "":
+            alt_label = dsid
+        mime = magic.Magic(mime=True)
+        upload_file = {
+            "file": (file, open(file, "rb"), mime.from_file(file), {"Expires": "0"})
+        }
+        r = requests.post(
+            f"{self.fedora_url}/fedora/objects/{pid}/datastreams/{dsid}/?controlGroup=M&dsLabel={alt_label}&versionable="
+            f"{versionable}&dsState={datastream_state}&checksumType={checksum_type}",
+            auth=self.auth,
+            files=upload_file,
+        )
+        if r.status_code == 201:
+            return r.status_code
+        else:
+            raise Exception(
+                f"\nFailed to create {dsid} datastream on {pid} with {file} as content. Fedora returned this"
+                f"status code: {r.status_code}."
+            )
+
+    def add_policy(self, pid):
+        # Make sure to set this first
+        policy = self.add_managed_datastream(pid, "POLICY", "policies/NAGPRA_POLICY.xml")
+        if policy == "":
+            raise Exception(
+                f"\nFailed to create POLICY on {pid}."
+            )
+        return policy
+
+
+if __name__ == "__main__":
+    with open('to_restrict.txt', 'r') as to_restrict:
+        for pid in to_restrict:
+            print(pid.strip())
