82dc6a9c3f09605da107a70376b7f1ecbd77ccbf
Mark Baggett <mbagget1@utk.edu>
2023-12-12 14:47:57 -0500

    Refactor.

--- generate_dc_with_saxon.py
+++ generate_dc_with_saxon.py
@@ -1,17 +1,10 @@
 import subprocess
 import os
 
-directory_path = "/Users/markbaggett/metadata/delaney/cleaned_data/bookLevelRecords/mark_metadata"
-output_path = "/Users/markbaggett/metadata/delaney/cleaned_data/bookLevelRecords/mark_dc"
+directory_path = "/Users/markbaggett/metadata/wallace/cleaned-data/modsxml/compounds/real"
+output_path = "/Users/markbaggett/metadata/wallace/cleaned-data/modsxml/compounds/real_dc"
 transform = "/Users/markbaggett/PycharmProjects/dumptruck/transform.xsl"
 saxon = "/Users/markbaggett/PycharmProjects/dumptruck/saxon.jar"
-
-# Define the command you want to ru
-# Example: command = "convert -resize 50% input.jpg output.jpg"
-
-# Directory containing the files
-
-# Loop through the files in the directory
 for filename in os.listdir(directory_path):
     if os.path.isfile(os.path.join(directory_path, filename)):
         # Build the full path to the file
